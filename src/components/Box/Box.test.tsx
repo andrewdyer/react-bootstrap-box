1f97c538ff3b74f6e8f5f81293321d07fc37b602
andrewdyer <andrewdyer@outlook.com>
2024-08-01 21:28:49 +0100

    feat: :sparkles: add support for custom HTML tags in Box component
    - Modify Box component to accept an 'as' prop
    - Default to 'div' if no 'as' prop is provided
    - Allow users to specify different HTML tags (e.g., span, section) using the 'as' prop

--- src/components/Box/Box.test.tsx
+++ src/components/Box/Box.test.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Box from './Box';
+import Box, { type BoxProps } from './Box';
+
+const renderBox = (props?: Partial<BoxProps>) => render(<Box {...props}>Hello, World!</Box>);
 
 describe('Box', () => {
     test('renders box component', () => {
-        render(<Box>Hello, World!</Box>);
+        renderBox();
 
         const BoxElement = screen.getByText(/Hello, World!/i);
 
         expect(BoxElement).toBeInTheDocument();
+    });
+
+    test('renders box component as custom element', () => {
+        renderBox({ as: 'section' });
+
+        const BoxElement = screen.getByText(/Hello, World!/i);
+
+        expect(BoxElement).toBeInTheDocument();
+        expect(BoxElement.tagName).toBe('SECTION');
     });
 });
--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export interface BoxProps extends React.HTMLAttributes<HTMLElement> {}
+export interface BoxProps extends React.HTMLAttributes<HTMLElement> {
+    as?: keyof React.ReactHTML;
+}
 
-const Box: React.FC<BoxProps> = ({ ...props }) => {
-    return <div {...props} />;
+const Box: React.FC<BoxProps> = ({ as: Component = 'div', ...props }) => {
+    return <Component {...props} />;
 };
 
 export default Box;
