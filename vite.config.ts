0ab5b7bce329178494393dfd75a5d1397c05ba66
Andrew Dyer <andrewdyer@outlook.com>
2025-09-01 22:42:03 +0100

    chore: :wrench: migrated project to a Vite-based setup

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist/types',
+      tsconfigPath: path.resolve(__dirname, 'tsconfig.build.json'),
+      insertTypesEntry: true,
+      copyDtsFiles: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'react-bootstrap-box',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/setupTests.ts'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      reportsDirectory: 'coverage',
+      provider: 'v8',
+    },
+  },
+});
